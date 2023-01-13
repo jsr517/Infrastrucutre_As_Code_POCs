74dff55962f366dc40827b6d46c5d669d545a972
JarnailSingh <zeal517@gmail.com>
2023-01-12 19:22:41 -0800

    added subnets

--- create_resource_VPC.tf
+++ create_resource_VPC.tf
@@ -10,7 +10,6 @@ variable "subnet_prefix" {
 }
 
 
-
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -18,24 +17,24 @@ resource "aws_vpc" "prod-vpc" {
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = var.subnet_prefix[0].cidr_block
-  availability_zone = "us-east-1a"
+# resource "aws_subnet" "subnet-1" {
+#   vpc_id            = aws_vpc.prod-vpc.id
+#   cidr_block        = var.subnet_prefix[0].cidr_block
+#   availability_zone = "us-east-1a"
 
-  tags = {
-    Name = var.subnet_prefix[0].name
-  }
-}
+#   tags = {
+#     Name = var.subnet_prefix[0].name
+#   }
+# }
 
-resource "aws_subnet" "subnet-2" {
-  vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = var.subnet_prefix[1].cidr_block
-  availability_zone = "us-east-1a"
+# resource "aws_subnet" "subnet-2" {
+#   vpc_id            = aws_vpc.prod-vpc.id
+#   cidr_block        = var.subnet_prefix[1].cidr_block
+#   availability_zone = "us-east-1a"
 
-  tags = {
-    Name = var.subnet_prefix[1].name
-  }
-}
+#   tags = {
+#     Name = var.subnet_prefix[1].name
+#   }
+# }
 
 
